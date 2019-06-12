7003161ddc94e725e0d7943efa7757cce566d0b3
EBS\radu.lungu <radu.lungu@nttdata.ro>
2019-06-12 04:50:36 +0300

    added validation for login, register and add contact

--- filtered_contacts.controller.php
+++ filtered_contacts.controller.php
@@ -1,8 +1,160 @@
 <?php
 
+	require_once 'database_conn.php';
+
+	require_once 'filtered_contacts.model.php';
+
+	require_once 'test_input.php';
+
+    $first_name_error_filter = $last_name_error_filter = $phone_number_error_filter = $email_error_filter = $min_age_error_filter = $max_age_error_filter = $city_error_filter = $faculty_error_filter = $interests_error_filter = '';
+
+    $first_name_filter = $last_name_filter = $phone_number_filter = $email_filter = $min_age_filter = $max_age_filter = $city_filter = $faculty_filter = $interests_filter = '';
+
     if (isset($_POST['submit_filter'])) {
         
-        $allFilteredContacts=filterContacts($conn);
+
+        if (empty($_POST["user_fname"])) {
+            
+            $first_name_filter = "";
+
+        } else {
+        
+            $first_name_filter = test_input($_POST["user_fname"]);
+            
+            if (!preg_match("/^[a-zA-Z ]*$/",$first_name_filter)) {
+                
+                $first_name_error_filter = "Only letters and white space allowed!"; 
+            }
+        }
+
+
+        if (empty($_POST["user_lname"])) {
+            
+            $last_name_filter = "";
+
+        } else {
+        
+            $last_name_filter = test_input($_POST["user_lname"]);
+            
+            if (!preg_match("/^[a-zA-Z ]*$/",$last_name_filter)) {
+                
+                $last_name_error_filter = "Only letters and white space allowed!";
+            }
+        }
+
+
+        if (empty($_POST["phone_nr"])) {
+            
+            $phone_number_filter = "";
+
+        } else {
+        
+            $phone_number_filter = test_input($_POST["phone_nr"]);
+            
+            if (!preg_match("/^[0-9]{10}+$/",$phone_number_filter)) {
+
+                $phone_number_error_filter = "The format of your phone number is not valid!";
+            }
+        }
+
+
+        if (empty($_POST["mail"])) {
+            
+            $email_filter = "";
+
+        } else {
+        
+            $email_filter = test_input($_POST["mail"]);
+            
+            if (!filter_var($email_filter, FILTER_VALIDATE_EMAIL)) {
+                
+                $email_error_filter = "Invalid email format!";
+            }
+        }
+
+
+        if (empty($_POST["user_min_age"])) {
+            
+            $min_age_filter = "";
+
+        } else {
+        
+            $min_age_filter = test_input($_POST["user_min_age"]);
+            
+            if (!preg_match("/^[0-9]+$/",$min_age_filter)) {
+                
+                $min_age_error_filter = "Only digits allowed!";
+            }
+        }
+
+
+        if (empty($_POST["user_max_age"])) {
+            
+            $max_age_filter = "";
+
+        } else {
+        
+            $max_age_filter = test_input($_POST["user_max_age"]);
+            
+            if (!preg_match("/^[0-9]+$/",$max_age_filter)) {
+                
+                $max_age_error_filter = "Only digits allowed!";
+            }
+        }
+
+
+        if (empty($_POST["user_cityaddress"])) {
+            
+            $city_filter = "";
+
+        } else {
+        
+            $city_filter = test_input($_POST["user_cityaddress"]);
+            
+            if (!preg_match("/^[a-zA-Z ]*$/",$city_filter)) {
+                
+                $city_error_filter = "Only letters and white space allowed!"; 
+            }
+        }
+
+
+        if (empty($_POST["what_faculty"])) {
+            
+            $faculty_filter = "";
+
+        } else {
+
+            $faculty_filter = test_input($_POST["what_faculty"]);
+        
+            if (!preg_match("/^[a-zA-Z ]*$/",$faculty_filter)) {
+          
+                $faculty_error_filter = "Only letters and white space allowed!";
+            }    
+        }
+
+
+        if (empty($_POST["interests"])) {
+            
+            $interests_filter = "";
+
+        } else {
+
+            $interests_filter = test_input($_POST["interests"]);
+        
+            if (!preg_match("/^[a-zA-Z ,]*$/",$interests_filter)) {
+          
+                $interests_error_filter = "Only letters, white spaces and commas allowed!"; 
+            }    
+        }
+
+        if ($first_name_error_filter === '' && $last_name_error_filter === '' && $phone_number_error_filter === '' && $email_error_filter === '' && $min_age_error_filter === '' && $max_age_error_filter === '' && $city_error_filter === '' && $faculty_error_filter === '' && $interests_error_filter === '') {
+
+        	$allFilteredContacts = filterContacts($conn);
+        } else {
+
+        	header('Location: /index.php?action=filterContacts');
+        }
+
     }
 
 ?>
--- index.controller.php
+++ index.controller.php
@@ -1,5 +1,18 @@
 <?php
 
+    require_once 'database_conn.php';
+
+    include_once 'index.model.php';
+
+    include_once 'index.php';
+
+    require_once 'test_input.php';
+
+    $first_name_error = $last_name_error = $phone_number_error = $email_error = $city_error = $age_error = $faculty_error = $web_address_error = $interests_error = '';
+
+    $first_name = $last_name = $phone_number = $email = $city = $age = $faculty = $web_address = $interests = '';
+
+
     if (isset($_POST['create_group_button'])) {
 
         $create_group_response = createNewGroup($conn, $_SESSION['user_id'], $_POST['create_group']);
@@ -19,7 +32,7 @@
     }
             
     if (isset($_POST['button_addcontact'])) {
-        
+    
         $avatarPath = "";
         
         $target_dir = "uploads/";
@@ -46,8 +59,147 @@
             
             echo "Sorry, your file was not uploaded.";
         }
+    
+
+
+        if (empty($_POST["first_name"])) {
+            
+            $first_name_error = "First name is required!";
+
+        } else {
         
-        $add_contact_response = addContact($conn, $_SESSION['user_id'],  $_POST['first_name'], $_POST['last_name'], $_POST['phone_number'], $_POST['user_email'], $_POST['user_city'], $_POST['user_age'], $_POST['user_faculty'], $_POST['user_web'], $_POST['user_interests'], $avatarPath);
+            $first_name = test_input($_POST["first_name"]);
+            
+            if (!preg_match("/^[a-zA-Z ]*$/",$first_name)) {
+                
+                $first_name_error = "Only letters and white space allowed!"; 
+            }
+        }
+
+
+        if (empty($_POST["last_name"])) {
+            
+            $last_name_error = "Last name is required!";
+
+        } else {
+        
+            $last_name = test_input($_POST["last_name"]);
+            
+            if (!preg_match("/^[a-zA-Z ]*$/",$last_name)) {
+                
+                $last_name_error = "Only letters and white space allowed!";
+            }
+        }
+
+
+        if (empty($_POST["phone_number"])) {
+            
+            $phone_number_error = "Phone number is required!";
+
+        } else {
+        
+            $phone_number = test_input($_POST["phone_number"]);
+            
+            if (!preg_match("/^[0-9]{10}+$/",$phone_number)) {
+
+                $phone_number_error = "The format of your phone number is not valid!";
+            }
+        }
+
+
+        if (empty($_POST["user_email"])) {
+            
+            $email_error = "Email is required!";
+
+        } else {
+        
+            $email = test_input($_POST["user_email"]);
+            
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                
+                $email_error = "Invalid email format!";
+            }
+        }
+
+
+        if (empty($_POST["user_city"])) {
+            
+            $city_error = "City is required!";
+
+        } else {
+        
+            $city = test_input($_POST["user_city"]);
+            
+            if (!preg_match("/^[a-zA-Z ]*$/",$city)) {
+                
+                $city_error = "Only letters and white space allowed!"; 
+            }
+        }
+
+
+        if (empty($_POST["user_age"])) {
+            
+            $age_error = "Age is required!";
+
+        } else {
+        
+            $age = test_input($_POST["user_age"]);
+            
+            if (!preg_match("/^[0-9]+$/",$age)) {
+                
+                $age_error = "Only digits allowed!";
+            }
+        }
+
+        
+        if (empty($_POST["user_faculty"])) {
+            
+            $faculty = "";
+
+        } else {
+
+            $faculty = test_input($_POST["user_faculty"]);
+        
+            if (!preg_match("/^[a-zA-Z ]*$/",$faculty)) {
+          
+                $faculty_error = "Only letters and white space allowed!";
+            }    
+        }
+
+
+        if (empty($_POST["user_web"])) {
+            
+            $web_address = "";
+
+        } else {
+
+            $web_address = test_input($_POST["user_web"]);
+        
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$web_address)) {
+          
+                $web_address_error = "Invalid URL!"; 
+            }    
+        }
+
+
+        if (empty($_POST["user_interests"])) {
+            
+            $interests = "";
+
+        } else {
+
+            $interests = test_input($_POST["user_interests"]);
+        
+            if (!preg_match("/^[a-zA-Z ,]*$/",$interests)) {
+          
+                $interests_error = "Only letters, white spaces and commas allowed!"; 
+            }    
+        }
+
+        if ($first_name_error === '' && $last_name_error === '' && $phone_number_error === '' && $email_error === '' && $city_error === '' && $age_error === '' && $faculty_error === '' && $web_address_error === '' && $interests_error === '') {
+
+            $add_contact_response = addContact($conn, $_SESSION['user_id'],  $_POST['first_name'], $_POST['last_name'], $_POST['phone_number'], $_POST['user_email'], $_POST['user_city'], $_POST['user_age'], $_POST['user_faculty'], $_POST['user_web'], $_POST['user_interests'], $avatarPath);
+        }
     }
 
     if (isset($_POST['logout_button'])) {
